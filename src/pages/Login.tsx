1d6cade6e3b5e83155a32534eea973c4430413fa
Lido001 <146964211+Lido001@users.noreply.github.com>
2025-03-12 16:28:49 +0530

    final commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -80,20 +80,22 @@ const Login: React.FC = () => {
             <label className="block text-gray-700">Email</label>
             <input
               type="email"
-              className="w-full p-2 border border-gray-300 rounded mt-1"
+              className="w-full p-2 border border-gray-300 rounded mt-1 outline-0"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
+              autoComplete="off"
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700">Password</label>
             <input
               type="password"
-              className="w-full p-2 border border-gray-300 rounded mt-1"
+              className="w-full p-2 border border-gray-300 rounded mt-1 outline-0"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
+              autoComplete="off"
             />
           </div>
           <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded">
